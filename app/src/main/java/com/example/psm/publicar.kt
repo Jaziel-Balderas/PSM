0189d0af0816a464a5e16fa77adf5b9e2c7bd70a
Jaziel-Balderas <152482563+Jaziel-Balderas@users.noreply.github.com>
2025-10-19 22:50:53 -0600

    PANTALLA NOTIS

---
+++ app/src/main/java/com/example/psm/publicar.kt
@@ -0,0 +1,20 @@
+package com.example.psm
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class publicar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_publicar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
